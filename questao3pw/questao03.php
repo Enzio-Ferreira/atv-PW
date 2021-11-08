cfc0a24a93ace489f6178164990323d7c74b727f
Enzio-Ferreira <enzio.gomes@aluno.ce.gov.br>
2021-11-08 14:00:47 -0200

    Criar do repositório

---
+++ questao3pw/questao03.php
@@ -0,0 +1,19 @@
+<?php
+
+$clientes = array(
+"José" => array("Idade"=>25,"CPF"=>"087883456-18","RG"=>"2006089107117"),
+"Maria" => array("Idade"=>18,"CPF"=>"057825456-15","RG"=>"2001089097112"),
+"Ana" => array("Idade"=>24,"CPF"=>"037825445-30","RG"=>"2019089097118"),
+);
+
+echo "RG de Maria: ".$clientes["Maria"]["RG"]."<br>";
+echo "Idade de Ana: ".$clientes["Ana"]["Idade"]."<br>";
+
+echo "<br>";
+
+foreach ($clientes as $clients => $dados) {
+	echo $clients."<br>";
+	foreach ($dados as $key => $value) {
+		echo $key." - ".$value."<br>";
+	}
+}
---
+++ questao4pw/insert.php
@@ -0,0 +1,7 @@
+<?php
+
+$Nome = $_POST["nome"];
+$Email = $_POST["email"];
+$Idade = $_POST["idade"];
+
+echo $Nome."<br>".$Email."<br>".$Idade;
